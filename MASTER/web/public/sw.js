b51ceff0cfb2721caad58e5f5ec1681c03ecb51d
dev <dev@pub4>
2026-05-11 22:00:04 +0200

    3D depth: euler projection, per-zone z, anchor offsets, swarm refinements

--- MASTER/web/public/sw.js
+++ MASTER/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'master-v5';
+const CACHE = 'master-v6';
 const SHELL = ['/', '/canvas', '/swarm.html', '/icon.png', '/icon.svg', '/manifest.json'];
 const NETWORK_ONLY = /^\/(chat\/message|canvas\/stream|api\/)/;
 
