01465a56b63918d27fb692ff034bd057bf41451c
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-14 01:24:00 +0200

    Expand Current attributes for domain context

--- DEPLOY/rails/brgen/app/app/models/current.rb
+++ DEPLOY/rails/brgen/app/app/models/current.rb
@@ -1,4 +1,11 @@
 class Current < ActiveSupport::CurrentAttributes
+  attribute :city
+  attribute :country
+  attribute :currency
+  attribute :domain
+  attribute :locale
+  attribute :neighborhood
   attribute :session
+  attribute :subapp
   attribute :user
 end
