ddf682d3fe151e1ca4e81e79107b57494b39b034
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-13 22:11:59 +0200

    Add runtime event record primitive

---
+++ MASTER/lib/master/runtime/event_record.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "securerandom"
+require "time"
+
+module Master
+  module Runtime
+    EventRecord = Struct.new(:id, :timestamp, :workflow_id, :event, :payload, keyword_init: true) do
+      def self.build(event:, workflow_id: nil, payload: {})
+        new(
+          id: SecureRandom.uuid,
+          timestamp: Time.now.utc.iso8601(6),
+          workflow_id: workflow_id,
+          event: event.to_s,
+          payload: payload || {}
+        )
+      end
+
+      def to_h
+        {
+          id: id,
+          timestamp: timestamp,
+          workflow_id: workflow_id,
+          event: event,
+          payload: payload
+        }.compact
+      end
+    end
+  end
+end
