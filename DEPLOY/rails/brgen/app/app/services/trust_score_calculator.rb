15406234d2fda64c87bc7c7714810f1f1642bc75
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-14 03:03:44 +0200

    Add trust score calculator

---
+++ DEPLOY/rails/brgen/app/app/services/trust_score_calculator.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class TrustScoreCalculator
+  SIGNAL_WEIGHTS = {
+    "account_created" => 5,
+    "email_verified" => 10,
+    "phone_verified" => 20,
+    "bankid_verified" => 100,
+    "merchant_verified" => 150,
+    "successful_trade" => 15,
+    "post_removed" => -20,
+    "spam_report" => -40,
+    "moderation_ban" => -200
+  }.freeze
+
+  def initialize(user:, scope: "global")
+    @scope = scope
+    @user = user
+  end
+
+  def call
+    score = user.trust_signals.sum do |signal|
+      SIGNAL_WEIGHTS.fetch(signal.kind, signal.weight)
+    end
+
+    reputation_score = user.reputation_scores.find_or_initialize_by(scope: scope)
+    reputation_score.update!(
+      calculated_at: Time.current,
+      score: score
+    )
+
+    reputation_score
+  end
+
+  private
+
+  attr_reader :scope, :user
+end
