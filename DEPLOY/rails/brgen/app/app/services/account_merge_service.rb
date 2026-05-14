ccc9c368df048023953222bf0e0c06aaea437c8e
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-14 03:02:36 +0200

    Add guest account merge service

---
+++ DEPLOY/rails/brgen/app/app/services/account_merge_service.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class AccountMergeService
+  def initialize(guest_user:, user:)
+    @guest_user = guest_user
+    @user = user
+  end
+
+  def call
+    return user if guest_user == user
+    return user unless guest_user.guest?
+
+    ActiveRecord::Base.transaction do
+      merge_posts
+      merge_comments
+      merge_conversations
+      merge_trust_signals
+      merge_moderation_flags
+      merge_sessions
+
+      AccountMerge.create!(
+        guest_user: guest_user,
+        user: user,
+        status: "merged",
+        merged_at: Time.current
+      )
+
+      guest_user.update!(guest: false)
+    end
+
+    user
+  end
+
+  private
+
+  attr_reader :guest_user, :user
+
+  def merge_comments
+    guest_user.comments.update_all(user_id: user.id)
+  end
+
+  def merge_conversations
+    guest_user.conversation_participants.update_all(user_id: user.id)
+  end
+
+  def merge_moderation_flags
+    guest_user.moderation_flags.update_all(user_id: user.id)
+  end
+
+  def merge_posts
+    guest_user.posts.update_all(user_id: user.id)
+  end
+
+  def merge_sessions
+    guest_user.sessions.update_all(user_id: user.id)
+  end
+
+  def merge_trust_signals
+    guest_user.trust_signals.update_all(user_id: user.id)
+  end
+end
