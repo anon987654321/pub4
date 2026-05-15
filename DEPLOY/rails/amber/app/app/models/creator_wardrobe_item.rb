ce9a4d8bed332a76375c6163b4b1de8a0bc68731
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:18:26 +0200

    Add Amber creator wardrobe item model

---
+++ DEPLOY/rails/amber/app/app/models/creator_wardrobe_item.rb
@@ -0,0 +1,7 @@
+class CreatorWardrobeItem < ApplicationRecord
+  belongs_to :creator_profile
+  belongs_to :item
+
+  validates :item_id, uniqueness: { scope: :creator_profile_id }
+  validates :caption, length: { maximum: 300 }
+end
