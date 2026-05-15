b1a0b9b79c71429767ecd71c7986c2ce0fc25410
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:32:29 +0200

    Add Amber background removal job

---
+++ DEPLOY/rails/amber/app/app/jobs/remove_background_job.rb
@@ -0,0 +1,9 @@
+class RemoveBackgroundJob < ApplicationJob
+  queue_as :default
+
+  def perform(item_id)
+    item = Item.find(item_id)
+    Rails.logger.info("Amber background-removal placeholder for item=#{item.id}")
+    item.update!(analysis_status: "background_removal_pending") if item.respond_to?(:analysis_status)
+  end
+end
