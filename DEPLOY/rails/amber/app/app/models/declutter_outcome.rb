86df58458c94e30b64ade5d95f21de6ac929556d
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 09:06:36 +0200

    Add Amber declutter outcome model

---
+++ DEPLOY/rails/amber/app/app/models/declutter_outcome.rb
@@ -0,0 +1,32 @@
+class DeclutterOutcome < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+
+  ACTIONS = %w[sold donated gifted recycled repaired archived released].freeze
+
+  validates :action, inclusion: { in: ACTIONS }
+  validates :notes, length: { maximum: 1_000 }
+  validates :amount_recovered, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  before_validation :assign_user
+  after_create :sync_item_lifecycle
+
+  private
+
+  def assign_user
+    self.user ||= item&.user
+  end
+
+  def sync_item_lifecycle
+    state = case action
+            when "sold" then "sold"
+            when "donated" then "donated"
+            when "gifted", "released" then "released"
+            when "recycled" then "recycled"
+            when "repaired" then "active"
+            when "archived" then "sentimental_archive"
+            else item.lifecycle_state
+            end
+    item.update!(lifecycle_state: state)
+  end
+end
