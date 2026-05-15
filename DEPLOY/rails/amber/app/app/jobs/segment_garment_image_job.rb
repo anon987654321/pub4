3590fd11cd2619e6814d39a26b6ab450e498c34b
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:31:58 +0200

    Add Amber garment segmentation job

---
+++ DEPLOY/rails/amber/app/app/jobs/segment_garment_image_job.rb
@@ -0,0 +1,9 @@
+class SegmentGarmentImageJob < ApplicationJob
+  queue_as :default
+
+  def perform(item_id)
+    item = Item.find(item_id)
+    Rails.logger.info("Amber segmentation placeholder for item=#{item.id}")
+    item.update!(analysis_status: "segmentation_pending") if item.respond_to?(:analysis_status)
+  end
+end
