22f145dcd3901c8ccc8b7f1deb9a8d3d45ed820f
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:19:17 +0200

    Add Amber sustainability metric model

---
+++ DEPLOY/rails/amber/app/app/models/sustainability_metric.rb
@@ -0,0 +1,12 @@
+class SustainabilityMetric < ApplicationRecord
+  belongs_to :item
+
+  validates :resale_value, :repair_cost_estimate, :environmental_score,
+            numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+
+  def unused?
+    item.times_worn.to_i.zero?
+  end
+
+  def cost_per_wear = item.cost_per_wear
+end
