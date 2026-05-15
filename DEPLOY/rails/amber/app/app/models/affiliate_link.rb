32ee4d0ab9e38896bfe28d47b156959d2ef60860
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:19:51 +0200

    Add Amber affiliate link model

---
+++ DEPLOY/rails/amber/app/app/models/affiliate_link.rb
@@ -0,0 +1,7 @@
+class AffiliateLink < ApplicationRecord
+  belongs_to :item
+
+  validates :url, :merchant, presence: true
+  validates :url, length: { maximum: 2_000 }
+  validates :commission_rate, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
+end
