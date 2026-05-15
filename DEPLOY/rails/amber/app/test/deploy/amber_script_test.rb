92c42b1aed6b3ec5838089266da5d24470741e92
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:54:00 +0200

    Add Amber deploy script tests

---
+++ DEPLOY/rails/amber/app/test/deploy/amber_script_test.rb
@@ -0,0 +1,28 @@
+require "test_helper"
+
+class AmberScriptTest < ActiveSupport::TestCase
+  SCRIPT = Rails.root.join("..", "amber.sh")
+
+  test "deploy script is configured for amber instead of a template placeholder" do
+    content = SCRIPT.read
+
+    assert_includes content, "APP_NAME=amber"
+    refute_includes content, "%APP_NAME%"
+    assert_includes content, "APP_DOMAIN=amber.brgen.no"
+  end
+
+  test "deploy script avoids self-copying an amber bundle cache" do
+    content = SCRIPT.read
+
+    assert_includes content, "SHARED_BUNDLE_CACHE"
+    assert_includes content, "${bundle_home} != /home/amber/.bundle"
+  end
+
+  test "deploy script uses modern bundler deployment configuration" do
+    content = SCRIPT.read
+
+    assert_includes content, "bundle config set --local deployment true"
+    assert_includes content, "bundle config set --local without 'development test'"
+    refute_includes content, "bundle install --deployment --without"
+  end
+end
