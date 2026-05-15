52aa4bd4db2a2ce24e0c517bc2b73feff91da567
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-15 08:17:50 +0200

    Add Amber privacy settings model

---
+++ DEPLOY/rails/amber/app/app/models/privacy_setting.rb
@@ -0,0 +1,8 @@
+class PrivacySetting < ApplicationRecord
+  belongs_to :user
+
+  enum :wardrobe_visibility, { wardrobe_private: "private", wardrobe_followers: "followers", wardrobe_public: "public" }, default: :wardrobe_private
+  enum :analytics_visibility, { analytics_private: "private", analytics_aggregate: "aggregate", analytics_public: "public" }, default: :analytics_private
+
+  def public_wardrobe? = wardrobe_public?
+end
