5899e4f0536bab7efa2f4d1f7e247642d956f1b6
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-24 08:19:21 +0200

    extract brgen playlist collaboration model

---
+++ DEPLOY/rails/brgen/app/models/playlist/collaboration.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Playlist
+  class Collaboration < ApplicationRecord
+    self.table_name = "playlist_collaborations"
+
+    ROLES = %w[owner editor viewer].freeze
+
+    belongs_to :user
+    belongs_to :set, class_name: "Playlist::Set", optional: true
+    belongs_to :playlist, class_name: "Playlist::Playlist", optional: true
+
+    validates :role, inclusion: { in: ROLES }, allow_blank: true
+    validates :user_id, uniqueness: { scope: %i[set_id playlist_id] }
+
+    before_validation :default_role
+
+    private
+
+    def default_role
+      self.role = "editor" if role.blank?
+    end
+  end
+end
