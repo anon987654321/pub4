a3be3f6a4f77322f6b0a4f3818c1191b542738b4
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-24 08:25:49 +0200

    extract brgen tv stream chat model

---
+++ DEPLOY/rails/brgen/app/models/tv/stream_chat.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Tv
+  class StreamChat < ApplicationRecord
+    self.table_name = "tv_stream_chats"
+
+    belongs_to :live_stream, class_name: "Tv::LiveStream"
+    belongs_to :user
+
+    validates :message, presence: true, length: { maximum: 1_000 }
+
+    scope :chronological, -> { order(:created_at) }
+
+    after_create_commit do
+      broadcast_append_later_to "tv:live_stream:#{live_stream_id}:entries"
+    end
+  end
+end
