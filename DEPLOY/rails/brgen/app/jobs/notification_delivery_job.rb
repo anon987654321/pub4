b4b30f2a9aca3626e5436d9fec97449ec37ecda4
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-24 05:56:33 +0200

    add brgen notification delivery job

---
+++ DEPLOY/rails/brgen/app/jobs/notification_delivery_job.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class NotificationDeliveryJob < ApplicationJob
+  queue_as :default
+
+  def perform(notification_id)
+    notification = Notification.find(notification_id)
+    notification.broadcast_prepend_to("brgen:notifications:#{notification.user_id}") if notification.respond_to?(:broadcast_prepend_to)
+    Shared::EventEmitter.call("brgen.notification.delivered", notification_id: notification.id, user_id: notification.user_id) if defined?(Shared::EventEmitter)
+  end
+end
