1b1eead2e4be64bda1d01a4513759ea7ee0a3f68
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-25 01:36:06 +0200

    Add converge engine specs

---
+++ MASTER/spec/converge/engine_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "tmpdir"
+require_relative "../../lib/converge"
+
+class ConvergeEngineSpec < Minitest::Test
+  def with_home
+    Dir.mktmpdir do |dir|
+      old_home = ENV["HOME"]
+      ENV["HOME"] = dir
+      yield
+    ensure
+      ENV["HOME"] = old_home
+    end
+  end
+
+  def test_runs_convergence
+    with_home do
+      engine = Converge::Engine.new(File.expand_path("../../data/converge_rules.yml", __dir__))
+      result = engine.run(code: "", reply_text: "plain reply")
+      assert result.key?(:violations)
+    end
+  end
+
+  def test_emits_events_to_subscribers
+    with_home do
+      engine = Converge::Engine.new(File.expand_path("../../data/converge_rules.yml", __dir__))
+      events = []
+      engine.subscribe { |event| events << event }
+      engine.run(code: "")
+      refute_empty events
+    end
+  end
+end
