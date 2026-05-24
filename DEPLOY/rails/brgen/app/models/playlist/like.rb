6858fe4cb8fe23f430f43e7052881b0d82f9cae4
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-24 08:20:18 +0200

    extract brgen playlist like model

---
+++ DEPLOY/rails/brgen/app/models/playlist/like.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Playlist
+  class Like < ApplicationRecord
+    self.table_name = "playlist_likes"
+
+    belongs_to :user
+    belongs_to :set, class_name: "Playlist::Set", optional: true
+    belongs_to :playlist, class_name: "Playlist::Playlist", optional: true
+
+    validates :user_id, uniqueness: { scope: %i[set_id playlist_id] }
+    validate :target_present
+
+    private
+
+    def target_present
+      errors.add(:base, "playlist target required") if set_id.blank? && playlist_id.blank?
+    end
+  end
+end
