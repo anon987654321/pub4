d5d5ff356e0af43232681b33e535f540ccf3787f
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-24 10:20:18 +0200

    add tv video notes controller

---
+++ DEPLOY/rails/brgen/app/controllers/tv/video_notes_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Tv
+  class VideoNotesController < ApplicationController
+    before_action :set_video
+
+    def create
+      note = @video.video_notes.build(video_note_params)
+      note.user = current_user if respond_to?(:current_user, true)
+      note.save!
+
+      respond_to do |format|
+        format.html { redirect_to tv_video_path(@video) }
+        format.turbo_stream
+        format.json { render json: { id: note.id }, status: :created }
+      end
+    end
+
+    private
+
+    def set_video
+      @video = Tv::Video.find(params[:video_id])
+    end
+
+    def video_note_params
+      params.require(:video_note).permit(:body, :timestamp)
+    end
+  end
+end
