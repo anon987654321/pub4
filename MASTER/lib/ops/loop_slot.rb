c48b64056042500d8e6d8d43a3d563ea608e3696
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-25 03:14:48 +0200

    Add loop slot status helpers

--- MASTER/lib/ops/loop_slot.rb
+++ MASTER/lib/ops/loop_slot.rb
@@ -15,6 +15,23 @@ module Master
         LOOP_FLAGS.select { |_name, env| ENV[env] == "1" }.keys
       end
 
+      def selected
+        enabled.first
+      end
+
+      def valid?
+        enabled.size <= 1
+      end
+
+      def status
+        {
+          selected: selected,
+          enabled: enabled,
+          valid: valid?,
+          flags: LOOP_FLAGS.transform_values { |env| ENV.fetch(env, "0") }
+        }
+      end
+
       def validate!
         active = enabled
         return true if active.size <= 1
@@ -22,10 +39,6 @@ module Master
         raise ArgumentError,
               "only one MASTER loop may run at a time; enabled=#{active.join(', ')}. " \
               "Set exactly one of MASTER_AUTOFIX=1, MASTER_WATCH=1, MASTER_WATCHER=1."
-      end
-
-      def selected
-        enabled.first
       end
     end
   end
