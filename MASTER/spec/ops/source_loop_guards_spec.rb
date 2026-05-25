21625a625101c35012af0ff6e3f9f62cb67c2422
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-25 04:13:17 +0200

    Add source loop guard tests

---
+++ MASTER/spec/ops/source_loop_guards_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+class SourceLoopGuardsSpec < Minitest::Test
+  ROOT = File.expand_path("../..", __dir__)
+
+  def read(rel)
+    File.read(File.join(ROOT, rel))
+  end
+
+  def test_heartbeat_source_requires_explicit_env
+    source = read("lib/loop/heartbeat.rb")
+    assert_includes source, 'return unless ENV["MASTER_HEARTBEAT"] == "1"'
+  end
+
+  def test_watcher_source_requires_explicit_env
+    source = read("lib/loop/watcher.rb")
+    assert_includes source, 'return unless ENV["MASTER_WATCHER"] == "1"'
+  end
+
+  def test_watch_loop_source_requires_explicit_env
+    source = read("lib/loop/watch_loop.rb")
+    assert_includes source, 'return unless ENV["MASTER_WATCH"] == "1"'
+  end
+end
