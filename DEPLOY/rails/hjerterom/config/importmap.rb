0ccee729b43a0f1ca0c9556c7013e809a807b4cc
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-05-28 19:06:30 +0200

    Pin hjerterom map module

--- DEPLOY/rails/hjerterom/config/importmap.rb
+++ DEPLOY/rails/hjerterom/config/importmap.rb
@@ -3,6 +3,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "hjerterom_map"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
