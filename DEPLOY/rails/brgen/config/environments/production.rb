b526af627f767f0e3c17a60b8080eb4dbdc4717d
dev <dev@brgen.openbsd.amsterdam>
2026-06-04 12:23:22 +0200

    brgen: add rc.d, SECRET_KEY_BASE env, drop stale credentials, fix port 38182

--- DEPLOY/rails/brgen/config/environments/production.rb
+++ DEPLOY/rails/brgen/config/environments/production.rb
@@ -3,6 +3,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
