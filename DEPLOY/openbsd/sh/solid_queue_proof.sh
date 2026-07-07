87b9eb6efc23a8f11ecd4b43a281069cde200f46
gktepsta <gktepsta@gmail.com>
2026-07-07 17:59:46 +0200

    chore(vps): close TTS debt; document solid queue follow-up

--- DEPLOY/openbsd/sh/solid_queue_proof.sh
+++ DEPLOY/openbsd/sh/solid_queue_proof.sh
@@ -8,4 +8,4 @@ app=${1:-}
 app_dir=/home/${app}/app
 [ -d "$app_dir" ] || { echo "missing $app_dir" >&2; exit 1; }
 
-su -m "$app" -c "export HOME=/home/${app}; cd ${app_dir} && bundle34 exec rails runner -e production \"n = SolidQueue::Process.count; puts \\\"solid_queue: ${app} processes=\\\${n}\\\"; exit(n.positive? ? 0 : 1)\""
+doas su -m "$app" -c "export HOME=/home/${app}; cd ${app_dir} && bundle34 exec rails runner -e production \"n = SolidQueue::Process.count; puts \\\"solid_queue: ${app} processes=\\\${n}\\\"; exit(n.positive? ? 0 : 1)\""
