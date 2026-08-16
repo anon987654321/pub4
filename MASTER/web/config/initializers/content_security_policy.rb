5a7750c923b04150366e18b89e75cebc49938870
gktepsta <gktepsta@gmail.com>
2026-08-16 17:14:07 +0200

    face: drop Google Inter from the chat document
    The face already falls through to the system stack. Loading Inter from
    fonts.googleapis.com was a third-party request on every primer paint
    and the only reason CSP still allowed that host.

--- MASTER/web/config/initializers/content_security_policy.rb
+++ MASTER/web/config/initializers/content_security_policy.rb
@@ -12,11 +12,11 @@ Rails.application.configure do
 
   config.content_security_policy do |policy|
     policy.default_src :self
-    policy.font_src :self, :data, "https://fonts.gstatic.com"
+    policy.font_src :self, :data
     policy.img_src :self, :data, "https://i.ytimg.com"
     policy.object_src :none
     policy.script_src :self, :blob, "https://www.youtube.com", "https://s.ytimg.com"
-    policy.style_src :self, :unsafe_inline, "https://fonts.googleapis.com"
+    policy.style_src :self, :unsafe_inline
     policy.connect_src :self, "https://www.youtube.com", "https://youtu.be"
     policy.media_src :self, :blob
     policy.frame_src "https://www.youtube.com", "https://www.youtube-nocookie.com"
