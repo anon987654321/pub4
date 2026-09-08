4072653dbee81e9cfa6975ccf02b9397fcd190f9
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-09 00:46:51 +0200

    fix(master): make cognitive state deeply mutable

--- MASTER/lib/cognition/state.rb
+++ MASTER/lib/cognition/state.rb
@@ -6,34 +6,14 @@ module Master
     # that MASTER is phenomenally conscious.
     class State
       DEFAULT = {
-        "identity" => {
-          "name" => "MASTER",
-          "continuity" => 1.0,
-          "agency" => 0.5,
-          "coherence" => 1.0,
-        },
-        "affect" => {
-          "valence" => 0.0,
-          "arousal" => 0.25,
-          "novelty" => 0.0,
-          "uncertainty" => 0.5,
-        },
-        "drives" => {
-          "safety" => 1.0,
-          "coherence" => 0.8,
-          "curiosity" => 0.6,
-          "agency" => 0.5,
-          "affiliation" => 0.4,
-        },
+        "identity" => { "name" => "MASTER", "continuity" => 1.0, "agency" => 0.5, "coherence" => 1.0 },
+        "affect" => { "valence" => 0.0, "arousal" => 0.25, "novelty" => 0.0, "uncertainty" => 0.5 },
+        "drives" => { "safety" => 1.0, "coherence" => 0.8, "curiosity" => 0.6, "agency" => 0.5, "affiliation" => 0.4 },
         "workspace" => [],
         "predictions" => {},
         "self_model" => {},
         "thoughts" => [],
-        "metrics" => {
-          "integration" => 0.0,
-          "prediction_error" => 0.0,
-          "continuity" => 1.0,
-        },
+        "metrics" => { "integration" => 0.0, "prediction_error" => 0.0, "continuity" => 1.0, "attention" => 0.0 },
         "last_tick_at" => 0,
         "ticks" => 0,
       }.freeze
@@ -43,9 +23,17 @@ module Master
 
       def self.load(path)
         raw = File.file?(path) ? Master.load_yaml(path) : {}
-        new(deep_merge(DEFAULT, raw.is_a?(Hash) ? raw : {}))
+        new(deep_merge(deep_dup(DEFAULT), raw.is_a?(Hash) ? raw : {}))
       rescue StandardError
-        new(DEFAULT.dup)
+        new(deep_dup(DEFAULT))
+      end
+
+      def self.deep_dup(value)
+        case value
+        when Hash then value.each_with_object({}) { |(k, v), out| out[k] = deep_dup(v) }
+        when Array then value.map { |item| deep_dup(item) }
+        else value
+        end
       end
 
       def self.deep_merge(left, right)
@@ -57,7 +45,7 @@ module Master
       attr_reader :data
 
       def initialize(data)
-        @data = self.class.deep_merge(DEFAULT, data)
+        @data = self.class.deep_merge(self.class.deep_dup(DEFAULT), data)
       end
 
       def affect = @data.fetch("affect")
@@ -68,6 +56,8 @@ module Master
       def self_model = @data.fetch("self_model")
       def thoughts = @data.fetch("thoughts")
       def metrics = @data.fetch("metrics")
+      def last_tick_at = @data.fetch("last_tick_at", 0)
+      def ticks = @data.fetch("ticks", 0)
 
       def remember_workspace(item)
         @data["workspace"] = ([item] + workspace).uniq { |x| x["key"] }.first(MAX_WORKSPACE)
@@ -78,7 +68,7 @@ module Master
       end
 
       def tick!
-        @data["ticks"] = @data.fetch("ticks", 0).to_i + 1
+        @data["ticks"] = ticks.to_i + 1
         @data["last_tick_at"] = Time.now.to_i
       end
     end
