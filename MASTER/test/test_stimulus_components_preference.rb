b4698d251b8291dd389135229a6a07df539d4d91
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 01:58:25 +0200

    test: cover upstream Stimulus Components catalogue

--- MASTER/test/test_stimulus_components_preference.rb
+++ MASTER/test/test_stimulus_components_preference.rb
@@ -16,6 +16,15 @@ class TestStimulusComponentsPreference < Minitest::Test
     assert_includes source, "severity: :soft"
   end
 
+  def test_catalog_covers_current_upstream_components
+    source = read("../RAILS/gates/lib/source/stimulus_components.rb")
+    %w[animated-number carousel chartjs color-picker confirmation dialog dropdown
+       glow hotkey lightbox places-autocomplete prefetch remote-rails reveal-controller
+       scroll-progress scroll-to sortable sound speech-recognition timeago].each do |name|
+      assert_includes source, name
+    end
+  end
+
   def test_existing_custom_controller_is_not_allowed_to_claim_component_ownership
     source = read("../RAILS/gates/lib/source/stimulus_components.rb")
     assert_includes source, "custom Stimulus behavior overlaps"
