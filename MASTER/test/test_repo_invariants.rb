fa5b6da88c343ae20a51933633980eead4879ac9
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 16:17:32 +0200

    refactor(master): consolidate repo micro-tests

---
+++ MASTER/test/test_repo_invariants.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "master"
+
+# frozen_string_literal: true
+class MasterPathsTest < Minitest::Test
+  def test_paths_resolve_under_master_root
+    assert MasterPaths.root.end_with?("/MASTER")
+    assert_equal File.expand_path("..", MasterPaths.root), MasterPaths.repo
+    assert MasterPaths.data("rules.yml").end_with?("/MASTER/data/rules.yml")
+    assert MasterPaths.state("provider_catalog.sqlite3").include?(".master")
+  end
+end
+
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require_relative "../lib/master"
+
+class RepoPathsTest < Minitest::Test
+  def test_repo_root_is_pub4_monorepo
+    assert_equal Master::REPO_ROOT, Master.repo_root
+    assert_equal File.expand_path("..", Master::ROOT), Master.repo_root
+  end
+
+  def test_tool_path_joins_under_master_tools
+    path = Master.tool_path("security_sweep.rb")
+    assert_includes path, "/MASTER/tools/security_sweep.rb"
+    assert File.file?(path)
+  end
+end
+
+# frozen_string_literal: true
+require "fileutils"
+
+class TestRepoMap < Minitest::Test
+  def test_brief_respects_token_budget
+    Dir.mktmpdir do |dir|
+      FileUtils.mkdir_p(File.join(dir, "lib"))
+      10.times { |index| File.write(File.join(dir, "lib", "file_#{index}.rb"), "puts :ok\n") }
+
+      rows = Master::Ground::Map::Repo.new(root: dir).brief("file", limit: 10, token_limit: 8)
+
+      assert rows.any?
+      assert_operator rows.join("\n").bytesize, :<=, 40
+    end
+  end
+end
+
