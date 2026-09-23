9c53583167f32fc8baf0793b17101c42f6991e79
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 20:26:39 +0200

    test(master): lock fix convergence wiring

---
+++ MASTER/test/test_fix_convergence_wiring.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+class FixConvergenceWiringTest < Minitest::Test
+  def test_fix_loop_wires_rendered_and_opportunity_evidence
+    loop_source = File.read(File.expand_path("../lib/fix/fix_loop.rb", __dir__))
+    runner_source = File.read(File.expand_path("../lib/fix/fix_loop/pass_runner.rb", __dir__))
+    rule_source = File.read(File.expand_path("../lib/fix/rule_loop.rb", __dir__))
+
+    assert_includes loop_source, 'require_relative "visual_pass"'
+    assert_includes loop_source, 'require_relative "opportunity_pass"'
+    assert_includes loop_source, "visual_pass:"
+    assert_includes loop_source, "opportunity_pass:"
+
+    assert_includes runner_source, "run_visual_pass"
+    assert_includes runner_source, "run_opportunity_pass"
+    assert_includes runner_source, "partition_findings"
+    assert_includes runner_source, "run_visual_stage"
+    assert_includes runner_source, "run_opportunity_stage"
+
+    assert_includes rule_source, "external_violations:"
+    assert_includes rule_source, "image:"
+    assert_includes rule_source, "@stage_commit"
+  end
+
+  def test_rendered_geometry_consumes_the_visual_measurement_payload
+    geometry = File.read(File.expand_path("../../RAILS/gates/lib/rendered/rendered_geometry.rb", __dir__))
+    walk = File.read(File.expand_path("../../RAILS/gates/support/geometry_probe/walk.js", __dir__))
+
+    assert_includes walk, "visual"
+    assert_includes walk, "first_screen"
+    assert_includes walk, "typography"
+    assert_includes geometry, "check_visual_composition"
+    assert_includes geometry, "principle=hierarchy"
+  end
+end
