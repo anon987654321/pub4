5a6691bf9cfdff7190a0705b92441c981934f4d0
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 19:04:22 +0200

    feat(master): keep rendered evidence through council synthesis

--- MASTER/lib/review/council/deliberation_synthesis.rb
+++ MASTER/lib/review/council/deliberation_synthesis.rb
@@ -14,8 +14,8 @@ module Master
           Result.err("council: veto from #{veto[:persona]}\n#{veto[:feedback]}", category: :validation)
         end
 
-        def append_judge_synthesis(feedback:, code:, context:)
-          synthesis = @judge_enabled ? judge(feedback:, code:, context:) : nil
+        def append_judge_synthesis(feedback:, code:, context:, image:)
+          synthesis = @judge_enabled ? judge(feedback:, code:, context:, image:) : nil
           return unless synthesis
 
           @bus&.publish(:council_synthesis, synthesis:)
@@ -54,7 +54,7 @@ module Master
           [base_context, "\nprior round:\n#{lines.join("\n")}\n"].compact.join
         end
 
-        def judge(feedback:, code:, context:)
+        def judge(feedback:, code:, context:, image: nil)
           @agent.ask(build_judge_prompt(feedback:, code:, context:))
         rescue StandardError => e
           @bus&.publish(:council_judge_error, error: e.message)
