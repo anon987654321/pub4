6baf965d8477d14e2598ed7c89c728f44637b79d
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 18:53:49 +0200

    test(master): expand recovery, restart, and known-good coverage

--- MASTER/test/test_reliability_second_tranche.rb
+++ MASTER/test/test_reliability_second_tranche.rb
@@ -67,6 +67,29 @@ class TestReliabilitySecondTranche < Minitest::Test
     end
   end
 
+  def test_known_good_rolls_clean_checkout_back_to_promoted_commit
+    Dir.mktmpdir("master-known-good") do |root|
+      init_git(root)
+      path = File.join(root, "a.rb")
+      File.write(path, "one\n")
+      git(root, "add", "a.rb")
+      git(root, "commit", "-m", "one")
+      good = git(root, "rev-parse", "HEAD")
+      Master::Ground::KnownGood.new(root:).promote!(commit: good)
+
+      File.write(path, "two\n")
+      git(root, "add", "a.rb")
+      git(root, "commit", "-m", "two")
+      assert_equal "two\n", File.read(path)
+
+      result = Master::Ground::KnownGood.new(root:).rollback!
+
+      assert result.ok?
+      assert_equal good, git(root, "rev-parse", "HEAD")
+      assert_equal "one\n", File.read(path)
+    end
+  end
+
   def test_known_good_rollback_refuses_dirty_tree
     Dir.mktmpdir("master-known-good") do |root|
       init_git(root)
@@ -89,6 +112,27 @@ class TestReliabilitySecondTranche < Minitest::Test
     end
   end
 
+  def test_service_supervisor_resets_restart_budget_after_recovery
+    Dir.mktmpdir("master-service") do |root|
+      healthy = false
+      starts = 0
+      supervisor = Master::Ground::ServiceSupervisor.new(root:)
+      first = supervisor.ensure(
+        name: "tts",
+        start: -> { starts += 1; healthy = true },
+        healthy: -> { healthy },
+        max_restarts: 2,
+        window_seconds: 60,
+        wait_seconds: 0,
+      )
+
+      assert first.ok?
+      assert first.value!.healthy?
+      assert_equal 1, starts
+      assert_empty supervisor.state("tts")["attempts"]
+    end
+  end
+
   def test_service_supervisor_stops_restart_storms
     Dir.mktmpdir("master-service") do |root|
       starts = 0
@@ -107,6 +151,21 @@ class TestReliabilitySecondTranche < Minitest::Test
       end
 
       assert_equal 2, starts
+    end
+  end
+
+  def test_fix_journal_preserves_the_original_deadline
+    Dir.mktmpdir("master-journal") do |root|
+      journal = Master::Fix::RunJournal.new(root:)
+      first = journal.start_or_resume(target: root, files: [], max_passes: 5, budget_seconds: 1)
+      journal.send(:persist, {
+        "version" => 1,
+        "runs" => [first.merge("deadline_at" => (Time.now.utc - 1).iso8601)],
+      })
+      resumed = journal.start_or_resume(target: root, files: [], max_passes: 5, budget_seconds: 1)
+
+      assert resumed["resumed"]
+      assert_operator resumed["remaining_seconds"], :<=, 0
     end
   end
 
