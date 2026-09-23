775a356f5e048b1700d9fef23e540a39a523afa3
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 20:26:36 +0200

    test(master): lock convergence opportunity detection

---
+++ MASTER/test/test_opportunity_pass.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "tmpdir"
+
+class OpportunityPassTest < Minitest::Test
+  def test_dead_rake_globs_are_real_findings
+    Dir.mktmpdir do |root|
+      master = File.join(root, "MASTER")
+      Dir.mkdir(master)
+      rakefile = File.join(master, "Rakefile")
+      File.write(rakefile, 'files = FileList["test/**/*_spec.rb"]' + "\n")
+
+      result = Master::Fix::OpportunityPass.new(root:).run(target: master, files: [])
+      findings = result.value.fetch(:findings)
+
+      assert_equal 1, findings.size
+      assert_includes findings.first[:message], "dead Rake glob"
+      assert_equal Master::Fix::OpportunityPass::RULE_ID, findings.first[:rule]
+    end
+  end
+
+  def test_parallel_tool_operator_implementations_are_real_findings
+    Dir.mktmpdir do |root|
+      master = File.join(root, "MASTER")
+      tool = File.join(master, "tools")
+      operator = File.join(master, "lib", "operator")
+      FileUtils.mkdir_p(tool)
+      FileUtils.mkdir_p(operator)
+      File.write(File.join(tool, "example.rb"), ("x = 1\n" * 81))
+      File.write(File.join(operator, "example.rb"), "module Operator; end\n")
+
+      result = Master::Fix::OpportunityPass.new(root:).run(target: master, files: [])
+      findings = result.value.fetch(:findings)
+
+      assert findings.any? { |finding| finding[:message].include?("parallel tool/operator") }
+    end
+  end
+end
