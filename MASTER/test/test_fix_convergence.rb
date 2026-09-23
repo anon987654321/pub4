aa85df7bbc136ab229d4d0c4cbade40d723c3eba
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 18:30:46 +0200

    test(master): require rendered review in fix convergence

--- MASTER/test/test_fix_convergence.rb
+++ MASTER/test/test_fix_convergence.rb
@@ -102,6 +102,12 @@ class TestFixConvergence < Minitest::Test
     assert_instance_of Master::Fix::FixLoop::CouncilRound, runner.instance_variable_get(:@council)
   end
 
+  def test_a_fix_loop_builds_a_rendered_reviewer
+    runner = build_loop([]).instance_variable_get(:@pass_runner)
+
+    assert_instance_of Master::Fix::RenderedReview, runner.instance_variable_get(:@rendered_review)
+  end
+
   # 9. The council argues inside the loop, and 11: its pick reaches the repair.
   def test_the_council_runs_inside_the_pass_and_its_picks_reach_the_repair
     council = Object.new
