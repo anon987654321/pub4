0a44d6e59ee49a8ed1c89252dbac7ce05fc50757
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-23 16:03:25 +0200

    refactor(master): close gate tree path references

--- RAILS/test/gates/constitutional_scan_targets_test.rb
+++ RAILS/test/gates/constitutional_scan_targets_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require_relative "../../../MASTER/gates/constitutional_scan"
+require_relative "../../../MASTER/lib/operator/gates"
 
 # The gate is four full MASTER scans, ~11 minutes end to end (brgen alone is
 # ~4.5). These cover which targets it picks, which is the part worth changing
