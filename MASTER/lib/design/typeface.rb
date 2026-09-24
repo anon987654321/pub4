32463c8745bae3a1b4963d20175b3e38501d9202
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 10:05:31 +0200

    refactor(design): source pairing compatibility rules from constitution

--- MASTER/lib/design/typeface.rb
+++ MASTER/lib/design/typeface.rb
@@ -38,10 +38,12 @@ module Master
         return true if display.to_s == body.to_s
         return false unless available?(display, root:) && available?(body, root:)
 
+        rules = Master.design("ultraminimalism", "typeface_rules", root:) || {}
         display_class = classification(display, root:)
         body_class = classification(body, root:)
         return true if relationship.to_s == "related_variable_family_pair"
-        return false if display_class == body_class && x_height(display, root:) == x_height(body, root:)
+        same_voice = display_class == body_class && x_height(display, root:) == x_height(body, root:)
+        return false if same_voice && rules.fetch("avoid_same_voice_pairing", true)
 
         true
       end
