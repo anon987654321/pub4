ce026956300483b86bc9310d7af638756237e9ec
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 06:18:02 +0200

    refactor: remove dating splash chrome

--- RAILS/brgen/test/controllers/dating_intro_test.rb
+++ RAILS/brgen/test/controllers/dating_intro_test.rb
@@ -6,7 +6,7 @@ require "test_helper"
 # request can see: the deck page mounts it, gives it both targets it reads, and
 # every action the markup names is a method the controller defines. Delete this
 # with the controller if the intro goes.
-class DatingIntroTest < ActionDispatch::IntegrationTest
+class DatingLandingTest < ActionDispatch::IntegrationTest
   CONTROLLER = Rails.root.join("engines/dating/app/javascript/controllers/dating_intro_controller.js")
 
   setup do
@@ -14,22 +14,11 @@ class DatingIntroTest < ActionDispatch::IntegrationTest
     host! "dating.brgen.no"
   end
 
-  test "the deck mounts the intro controller with the targets and actions it defines" do
-    source = File.read(CONTROLLER)
-    declared_targets = source[/static targets = \[([^\]]*)\]/, 1].to_s.scan(/"(\w+)"/).flatten
-    defined_methods = source.scan(/^  (\w+)\(/).flatten
-
+  test "the deck is the landing surface"
     get dating.root_path
     assert_response :success
-
-    assert_select "[data-controller~='dating-intro']", 1
-    %w[intro discover].each do |target|
-      assert_includes declared_targets, target
-      assert_select "[data-controller~='dating-intro'] [data-dating-intro-target='#{target}']", 1
-    end
-
-    actions = css_select("[data-action*='dating-intro#']").flat_map { |node| node["data-action"].scan(/dating-intro#(\w+)/).flatten }
-    refute_empty actions, "the intro section no longer wires any gesture to the controller"
-    assert_empty actions - defined_methods, "actions with no method in dating_intro_controller.js"
-  end
-end
+    assert_select ".dating-page-intro", 1
+    assert_select ".dating-discover-panel:not([hidden])", 1
+    assert_select "[data-controller~='dating-intro']", 0
+    assert_select "[data-controller~='swipe']", 1
+  endend
