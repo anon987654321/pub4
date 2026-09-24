c5d1bf5453afe59afbc303e183a30d397bbcc0b7
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 04:58:48 +0200

    test: lock Rails lint ratchet visibility

--- MASTER/test/test_ratchets.rb
+++ MASTER/test/test_ratchets.rb
@@ -29,6 +29,12 @@ class TestRatchets < Minitest::Test
                  "new debt against a recorded ceiling"
   end
 
+  def test_missing_rails_lints_are_not_silently_omitted
+    source = File.read(File.join(Operator::Ratchets::MASTER, "lib/operator/ratchets.rb"))
+    assert_includes source, 'lint file missing'
+    assert_includes source, 'Operator lint module missing'
+  end
+
   def test_missing_css_budget_is_not_silently_omitted
     source = File.read(File.join(Operator::Ratchets::MASTER, "lib/operator/ratchets.rb"))
     assert_includes source, 'name: "css_budget", current: nil, ceiling: nil'
