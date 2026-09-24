d2a887b9e0ae0fddd3a5a73d290e10359a6f3069
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 10:58:45 +0200

    visual: use shared gravity field pin

--- RAILS/brgen/app/javascript/application.js
+++ RAILS/brgen/app/javascript/application.js
@@ -1,6 +1,6 @@
 import "pub4/hotwire"
 import "controllers"
-import "gravity_field"
+import "pub4/gravity_field"
 
 if ("periodicSync" in navigator && "serviceWorker" in navigator) {
   navigator.serviceWorker.ready.then(reg => {
