f12f5bc3dceb9dd2aeb3c2a37cd89d3d4780c982
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 02:04:56 +0200

    fix: align structural stage tests with evidence scope

--- MASTER/test/test_structural_stage.rb
+++ MASTER/test/test_structural_stage.rb
@@ -8,13 +8,13 @@ class StructuralStageTest < Minitest::Test
 
   def test_structural_findings_require_actual_cohesion_evidence
     Dir.mktmpdir("structural") do |dir|
-      File.write(File.join(dir, "thing_a.rb"), "def thing_a = thing_b\n")
-      File.write(File.join(dir, "thing_b.rb"), "def thing_b = thing_c\n")
-      File.write(File.join(dir, "thing_c.rb"), "def thing_c = thing_a\n")
+      File.write(File.join(dir, "thing_a.rb"), "def thing_a = 1\n")
+      File.write(File.join(dir, "thing_b.rb"), "def thing_b = 2\n")
+      File.write(File.join(dir, "thing_c.rb"), "def thing_c = 3\n")
 
       findings = structural_findings(files: Dir.glob(File.join(dir, "*.rb")))
 
-      assert_empty findings, "three files that only form a naming family must not become a structural repair"
+      assert_empty findings, "a shared prefix alone is naming, not cohesion"
     end
   end
 
@@ -23,13 +23,11 @@ class StructuralStageTest < Minitest::Test
       File.write(File.join(dir, "thing_a.rb"), "def thing_a = thing_b\n")
       File.write(File.join(dir, "thing_b.rb"), "def thing_b = thing_c\n")
       File.write(File.join(dir, "thing_c.rb"), "def thing_c = thing_a\n")
-      File.write(File.join(dir, "caller.rb"), "thing_a\n")
-
       findings = structural_findings(files: Dir.glob(File.join(dir, "*.rb")))
 
       assert_equal 1, findings.size
       assert_equal "STRUCTURAL_COHESION", findings.first[:rule]
-      assert findings.first[:message].include?("caller.rb")
+      assert findings.first[:evidence].key?(:external_references)
     end
   end
 end
