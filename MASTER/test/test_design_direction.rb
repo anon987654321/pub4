9c08d4eb0575d6dd3febcb0784744d2a11ae632b
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 10:00:35 +0200

    test(design): cover marketplace composition alternatives

--- MASTER/test/test_design_direction.rb
+++ MASTER/test/test_design_direction.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+<sub># frozen_string_literal: true
 
 require_relative "test_helper"
 
@@ -28,3 +28,4 @@ class DesignDirectionTest < Minitest::Test
     assert_equal %w[product price headline proof action], spec.fetch("focal_order")
   end
 end
+</sub>
