a69068cc3d0c3d62138ebda4020584de42cb453f
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-24 10:26:24 +0200

    mobile: add registry contract tests

---
+++ RAILS/test/mobile_app_registry_test.rb
@@ -0,0 +1,92 @@
+# frozen_string_literal: true
+
+require "json"
+require "minitest/autorun"
+require_relative "../shared/lib/shared/mobile_app_registry"
+
+class MobileAppRegistryTest < Minitest::Test
+  REGISTRY = Shared::MobileAppRegistry
+
+  def test_registry_contains_core_verticals_and_amber
+    keys = REGISTRY.all.map(&:key)
+
+    assert_includes keys, :brgen
+    assert_includes keys, :radio
+    assert_includes keys, :dating
+    assert_includes keys, :tv
+    assert_includes keys, :takeaway
+    assert_includes keys, :marketplace
+    assert_includes keys, :maps
+    assert_includes keys, :messenger
+    assert_includes keys, :amber
+  end
+
+  def test_vertical_hosts_are_distinct_store_origins
+    hosts = REGISTRY.all.map(&:host)
+
+    assert_equal hosts.uniq.size, hosts.size
+    assert_equal "brgen.no", REGISTRY.fetch(:brgen).host
+    assert_equal "radio.brgen.no", REGISTRY.fetch(:radio).host
+    assert_equal "markedsplass.brgen.no", REGISTRY.fetch(:marketplace).host
+    assert_equal "amber.fashion", REGISTRY.fetch(:amber).host
+  end
+
+  def test_host_lookup_ignores_port_and_trailing_dot
+    assert_equal :brgen, REGISTRY.for_host("BRGEN.NO:443").key
+    assert_equal :amber, REGISTRY.for_host("amber.fashion.").key
+  end
+
+  def test_assetlinks_is_empty_until_a_real_signing_fingerprint_exists
+    previous = ENV.delete("BRGEN_ANDROID_CERT_SHA256")
+
+    begin
+      assert_equal "[]\n", REGISTRY.assetlinks_json("brgen.no")
+    ensure
+      ENV["BRGEN_ANDROID_CERT_SHA256"] = previous if previous
+    end
+  end
+
+  def test_assetlinks_uses_the_exact_package_and_configured_fingerprint
+    previous = ENV["BRGEN_ANDROID_CERT_SHA256"]
+    ENV["BRGEN_ANDROID_CERT_SHA256"] = "AA:BB,CC:DD"
+
+    begin
+      body = JSON.parse(REGISTRY.assetlinks_json("brgen.no"))
+
+      assert_equal ["delegate_permission/common.handle_all_urls"], body.fetch(0).fetch("relation")
+      target = body.fetch(0).fetch("target")
+      assert_equal "android_app", target.fetch("namespace")
+      assert_equal "no.brgen.brgen", target.fetch("package_name")
+      assert_equal ["AA:BB", "CC:DD"], target.fetch("sha256_cert_fingerprints")
+    ensure
+      ENV["BRGEN_ANDROID_CERT_SHA256"] = previous if previous
+    end
+  end
+
+  def test_apple_association_is_empty_until_the_team_id_exists
+    previous = ENV.delete("APPLE_TEAM_ID")
+
+    begin
+      body = JSON.parse(REGISTRY.apple_app_site_association("amber.fashion"))
+
+      assert_equal [], body.fetch("applinks").fetch("details")
+    ensure
+      ENV["APPLE_TEAM_ID"] = previous if previous
+    end
+  end
+
+  def test_apple_association_uses_the_exact_team_and_bundle_id
+    previous = ENV["APPLE_TEAM_ID"]
+    ENV["APPLE_TEAM_ID"] = "TEAM123"
+
+    begin
+      body = JSON.parse(REGISTRY.apple_app_site_association("amber.fashion"))
+      detail = body.fetch("applinks").fetch("details").fetch(0)
+
+      assert_equal "TEAM123.fashion.amber.app", detail.fetch("appID")
+      assert_equal ["*"], detail.fetch("paths")
+    ensure
+      ENV["APPLE_TEAM_ID"] = previous
+    end
+  end
+end
