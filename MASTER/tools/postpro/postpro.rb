26ba84bf1617b900cb86f02d0832bf8f3313e901
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-25 22:07:57 +0200

    fix: remove duplicated MASTER tools path

--- MASTER/tools/postpro/postpro.rb
+++ MASTER/tools/postpro/postpro.rb
@@ -10,9 +10,9 @@ require "time"
 require "fileutils"
 require "digest"
 # ../lib/io/... was correct at MASTER/tools/postpro/. It has not been correct
-# since the move to MASTER/tools/MASTER/tools/, where it resolves to MASTER/tools/MASTER/tools/lib/io/ -- a directory
+# since the move to MASTER/tools/, where it resolves to MASTER/tools/lib/io/ -- a directory
 # that does not exist -- so this file has aborted on its first require, every
-# invocation, since 687c07a43. MASTER/tools/MASTER/tools/dilla/dilla.rb reaches the same library
+# invocation, since 687c07a43. MASTER/tools/dilla/dilla.rb reaches the same library
 # by the path below; postpro and preprompt were never updated with it.
 require_relative "../../../lib/io/analog_capabilities"
 
