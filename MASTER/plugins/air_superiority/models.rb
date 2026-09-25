158c9f398881d5ec79b55ec6651c1d07a8cdc08c
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-25 21:09:00 +0200

    fix(master): avoid air superiority namespace collision

--- MASTER/plugins/air_superiority/models.rb
+++ MASTER/plugins/air_superiority/models.rb
@@ -2,7 +2,7 @@
 
 module Master
   module Plugins
-    module AirSuperiority
+    module AirSuperioritySupport
       Observation = Data.define(:kind, :data, :observed_at)
       Finding = Data.define(:kind, :severity, :details, :data, :observed_at)
       ScanResult = Data.define(:wifi, :bluetooth, :errors, :complete, :observed_at)
