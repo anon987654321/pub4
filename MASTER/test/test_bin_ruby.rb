9fa0f70d88a6ed27c753fba270915bd82b80987f
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-25 10:24:53 +0200

    test(master): pin runtime precedence

--- MASTER/test/test_bin_ruby.rb
+++ MASTER/test/test_bin_ruby.rb
@@ -47,6 +47,39 @@ class TestBinRuby < Minitest::Test
     end
   end
 
+
+  def test_ruby_runner_prefers_the_pinned_rbenv_over_generic_ruby34
+    old_path = ENV["PATH"]
+    old_fake_path = ENV["FAKE_RBENV_PATH"]
+
+    Dir.mktmpdir do |root|
+      fake_bin = File.join(root, "bin")
+      FileUtils.mkdir_p(fake_bin)
+      ruby_path = File.join(root, "ruby-3.4.9")
+      File.write(ruby_path, "#!/usr/bin/env ruby\\n")
+      File.chmod(0o755, ruby_path)
+      rbenv = File.join(fake_bin, "rbenv")
+      File.write(rbenv, <<~RUBY)
+        #!/usr/bin/env ruby
+        abort "wrong version" unless ENV["RBENV_VERSION"] == "3.4.9"
+        puts ENV.fetch("FAKE_RBENV_PATH")
+      RUBY
+      File.chmod(0o755, rbenv)
+      generic = File.join(fake_bin, "ruby34")
+      File.write(generic, "#!/bin/sh\\nexit 99\\n")
+      File.chmod(0o755, generic)
+      File.write(File.join(root, ".ruby-version"), "3.4.9\\n")
+
+      ENV["PATH"] = "#{fake_bin}#{File::PATH_SEPARATOR}#{old_path}"
+      ENV["FAKE_RBENV_PATH"] = ruby_path
+
+      assert_equal ruby_path, Operator::RubyRunner.ruby_cmd
+    ensure
+      ENV["PATH"] = old_path
+      ENV["FAKE_RBENV_PATH"] = old_fake_path
+    end
+  end
+
   def test_a_failing_script_keeps_its_exit_status
     _, _, status = unbundled { Open3.capture3(BIN, "-e", "exit 7") }
 
