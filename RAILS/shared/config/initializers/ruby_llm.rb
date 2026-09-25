cb49caa3262179b3be165cde1a24f4acda3791cf
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-25 20:49:58 +0200

    refactor(rails): remove unused RubyLLM global initializer

--- RAILS/shared/config/initializers/ruby_llm.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-return unless defined?(RubyLLM)
-
-RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPENAI_API_KEY"] || ENV["RUBY_LLM_OPENAI_API_KEY"]
-  config.anthropic_api_key = ENV["ANTHROPIC_API_KEY"] || ENV["RUBY_LLM_ANTHROPIC_API_KEY"]
-end
