5e4164b989273b09b1805ca533027efb7f1bb715
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-25 21:12:05 +0200

    fix(studio): repair master-relative paths after move

--- MASTER/tools/studio/postpro/postpro.rb
+++ MASTER/tools/studio/postpro/postpro.rb
@@ -14,7 +14,7 @@ require "digest"
 # that does not exist -- so this file has aborted on its first require, every
 # invocation, since 687c07a43. MASTER/tools/studio/dilla/dilla.rb reaches the same library
 # by the path below; postpro and preprompt were never updated with it.
-require_relative "../../MASTER/lib/io/analog_capabilities"
+require_relative "../../../lib/io/analog_capabilities"
 
 require "open3"
 require "rbconfig"
