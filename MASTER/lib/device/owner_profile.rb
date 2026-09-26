db8ec3724bf3c9c9c3b3e8d2ad566cb83db33f97
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-26 14:21:59 +0200

    feat: let each owner give MASTER a pet name

--- MASTER/lib/device/owner_profile.rb
+++ MASTER/lib/device/owner_profile.rb
@@ -6,9 +6,10 @@ module Master
   module Device
     module OwnerProfile
       extend Master::Io::AtomicWrite
-      KEYS = %w[name language locale timezone communication_style interests].freeze
+      KEYS = %w[name pet_name language locale timezone communication_style interests].freeze
       LABELS = {
         "name" => "Name",
+        "pet_name" => "MASTER pet name",
         "language" => "Language",
         "locale" => "Locale",
         "timezone" => "Timezone",
@@ -67,7 +68,7 @@ module Master
         return "owner0: profile complete" if missing.empty?
 
         labels = missing.first(3).map { |key| LABELS.fetch(key) }
-        "owner0: tell me any of these you want me to remember: #{labels.join(", ")}. "           "You can skip any item."
+        "owner0: tell me any of these you want me to remember: #{labels.join(", ")}. "           "You can skip any item. Give me a pet name too, if you want a more personal feel."
       end
 
       def ensure!(root:, subject:)
