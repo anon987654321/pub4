9c06af179bbc8457d482bd2a57935783e5d35c4b
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-26 14:37:23 +0200

    fix: carry keyboard sortable change into current main for sortable_keyboard_contract_test.rb

--- RAILS/test/sortable_keyboard_contract_test.rb
+++ RAILS/test/sortable_keyboard_contract_test.rb
@@ -8,7 +8,7 @@ class SortableKeyboardContractTest < Minitest::Test
   OUTFIT_VIEW = File.expand_path("../../amber/app/views/outfits/show.html.erb", __dir__)
   VARIANT_VIEW = File.expand_path("../../brgen/engines/marketplace/app/views/marketplace/variants/index.html.erb", __dir__)
 
-  def test_shared_sortable_has_a_keyboard_reorder_path
+  def test_shared_sortable_has_a_visible_keyboard_reorder_path
     source = File.read(VENDOR)
 
     assert_includes source, 'this.element.addEventListener("keydown", this.onKeyboardReorder)'
@@ -16,7 +16,11 @@ class SortableKeyboardContractTest < Minitest::Test
     assert_includes source, 'event.key !== "ArrowDown"'
     assert_includes source, 'item.tabIndex = 0'
     assert_includes source, 'item.setAttribute("aria-keyshortcuts", "ArrowUp ArrowDown")'
-    assert_includes source, 'await this.onUpdate({ item, newIndex })'
+    assert_includes source, 'this.installKeyboardControls()'
+    assert_includes source, 'button.textContent = delta < 0 ? "↑" : "↓"'
+    assert_includes source, 'this.identifier'
+    assert_includes source, 'moveBy'
+    assert_includes source, 'await this.onUpdate({ item, newIndex: nextIndex })'
   end
 
   def test_keyboard_contract_covers_both_existing_sortable_surfaces
@@ -24,7 +28,11 @@ class SortableKeyboardContractTest < Minitest::Test
     variants = File.read(VARIANT_VIEW)
 
     assert_includes outfit, 'data-controller="outfit-sortable"'
+    assert_includes outfit, 'data-sortable-move-up-label-value='
+    assert_includes outfit, 'data-sortable-move-down-label-value='
     assert_includes variants, 'data-controller="sortable"'
     assert_includes variants, 'data-sortable-resource-name-value="variant"'
+    assert_includes variants, 'data-sortable-move-up-label-value='
+    assert_includes variants, 'data-sortable-move-down-label-value='
   end
 end
