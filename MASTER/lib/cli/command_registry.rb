27cb25a8dc7e282d66bc45197ed4034c8adb0ef3
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-26 14:09:30 +0200

    fix: persist local owner for standing orders

--- MASTER/lib/cli/command_registry.rb
+++ MASTER/lib/cli/command_registry.rb
@@ -345,7 +345,11 @@ module Master
         fields = pairs.filter_map { |key, value| [ORDER_KEYS[key], value.to_s.strip] if ORDER_KEYS[key] }.to_h
         return ORDERS_USAGE unless fields[:name]
 
-        fields[:owner] ||= Fiber[:master_pair_subject].to_s.then { |paired| paired.empty? ? "operator" : paired }
+        fields[:owner] ||= begin
+          paired = Fiber[:master_pair_subject].to_s
+          paired = Master::Device::Agent.owner_subject(root:) if paired.empty? && defined?(Master::Device::Agent)
+          paired.empty? ? "operator" : paired
+        end
         standing.upsert(**fields)
       end
 
