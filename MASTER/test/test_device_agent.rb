28fed09973afb250c84d3c3096505e15f8debdcd
anon987654321 <62118265+anon987654321@users.noreply.github.com>
2026-09-26 14:16:48 +0200

    test: pin singular device ownership lifecycle

--- MASTER/test/test_device_agent.rb
+++ MASTER/test/test_device_agent.rb
@@ -53,6 +53,30 @@ class TestDeviceAgent < Minitest::Test
     end
   end
 
+  def test_claim_owner_refuses_a_second_owner
+    Dir.mktmpdir("device-agent") do |root|
+      Master::Device::Agent.send(:save_state, root, "owner_subject" => "owner123")
+      error = assert_raises(RuntimeError) { Master::Device::Agent.claim_owner!(root:, label: "Other") }
+      assert_match(/already paired/, error.message)
+    end
+  end
+
+  def test_release_owner_clears_identity_but_keeps_personal_workspace
+    Dir.mktmpdir("device-agent") do |root|
+      result = Master::Device::Agent.claim_owner!(root:, label: "Alex")
+      workspace = Master::Ground::PersonalWorkspace.dir_for(result[:subject], root:)
+      assert File.directory?(workspace)
+
+      released = Master::Device::Agent.release_owner!(root:)
+      assert_equal result[:subject], released
+      refute Master::Device::Agent.paired?(root:)
+      assert File.directory?(workspace)
+    ensure
+      Fiber[:master_paired] = nil
+      Fiber[:master_pair_subject] = nil
+    end
+  end
+
   def test_tick_persists_runtime_state_without_running_personal_orders
     Dir.mktmpdir("device-agent") do |root|
       cognition = FakeCognition.new
